0094614b6daf7d91ff83912b1489bfd65af7ff6f
sloproo <3440817+sloproo@users.noreply.github.com>
2025-12-03 17:11:36 +0200

    eka ygösel taas ��

--- 03/01.py
+++ 03/01.py
@@ -1,12 +1,21 @@
-akusto = "234234234234278"
-akut = 0
-for i in range(9, 0, -1):
-    ekan_paikka = akusto[:-1].find(str(i))
-    for j in range(i, 0, -1):
-        if str(j) in akusto[ekan_paikka + 1:]:
-            akut = int(str(i) + str(j))
+def joltit(akusto: str) -> int:
+    for i in range(9, 0, -1):
+        if (ekan_paikka := akusto[:-1].find(str(i))) >= 0:
+            eka = (i, ekan_paikka)
             break
-    if akut > 0:
-        break
-    
-print(akut)
+    if eka[1] == len(akusto) - 2:
+        toka = (int(akusto[-1]), len(akusto) - 1)
+    else:
+        for j in range(9, 0, -1):
+            if (tokan_paikka := akusto[eka[1] + 1:].find(str(j))) >= 0:
+                toka = (j, eka[1] + 1 + tokan_paikka)
+                break
+
+    return eka[0] * 10 + toka[0]
+
+jolttisumma = 0
+with open("input.txt") as f:
+    for r in f:
+        jolttisumma += joltit(r.strip())
+
+print(jolttisumma)
