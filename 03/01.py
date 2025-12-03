ffa69bf8f251618a4a3f644564d4ba01cc4724a1
sloproo <3440817+sloproo@users.noreply.github.com>
2025-12-03 11:10:52 +0200

    keskeneräinen töistä

--- 03/01.py
+++ 03/01.py
@@ -1,1 +1,12 @@
-# joo
+akusto = "234234234234278"
+akut = 0
+for i in range(9, 0, -1):
+    ekan_paikka = akusto[:-1].find(str(i))
+    for j in range(i, 0, -1):
+        if str(j) in akusto[ekan_paikka + 1:]:
+            akut = int(str(i) + str(j))
+            break
+    if akut > 0:
+        break
+    
+print(akut)
