37d120503a5b0295d9192559a8b3999273c359c6
sloproo <3440817+sloproo@users.noreply.github.com>
2025-12-05 08:02:05 +0200

    Vitospäivän eka alkuun

--- 05/01.py
+++ 05/01.py
@@ -1,1 +1,13 @@
-# alku
+with open("alku.txt") as f:
+    tuoreet = []
+    ainekset = []
+    for r in f:
+        if r == "\n":
+            break
+        tuoreet.append(tuple((int(luku) for luku in r.strip().split("-"))))
+    for r in f:
+        ainekset.append(int(r.strip()))
+
+    print(tuoreet)
+    print(ainekset)
+
