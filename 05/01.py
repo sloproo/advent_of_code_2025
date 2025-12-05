5830de90f618d048c3effd48ca649b0f34cf36a9
sloproo <3440817+sloproo@users.noreply.github.com>
2025-12-05 09:24:47 +0200

    Töissä korjattu
    Kyllä, näin on

--- 05/01.py
+++ 05/01.py
@@ -1,13 +1,24 @@
-with open("alku.txt") as f:
-    tuoreet = []
+with open("input.txt") as f:
+    tuoreiden_rajat = []
     ainekset = []
     for r in f:
         if r == "\n":
             break
-        tuoreet.append(tuple((int(luku) for luku in r.strip().split("-"))))
+        tuoreiden_rajat.append(tuple((int(luku) for luku in r.strip().split("-"))))
     for r in f:
         ainekset.append(int(r.strip()))
 
-    print(tuoreet)
+    print(tuoreiden_rajat)
     print(ainekset)
 
+tuoreet = []
+for aine in ainekset:
+    tuore = False
+    for (alku, loppu) in tuoreiden_rajat:
+        if aine >= alku and aine <= loppu:
+            tuore = True
+            break
+    if tuore:
+        tuoreet.append(aine)
+
+print(f"Tuoreita aineksia oli {len(tuoreet)}")
