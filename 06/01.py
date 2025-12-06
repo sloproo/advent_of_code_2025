94c56d9ea48faf12e27e9f707f07568f9163a613
sloproo <3440817+sloproo@users.noreply.github.com>
2025-12-06 11:29:27 +0200

    Itsenäisyyspäivän eka 🇫🇮

--- 06/01.py
+++ 06/01.py
@@ -1,1 +1,23 @@
-# alku
+from math import prod
+
+with open("input.txt") as f:
+    eka_rivi = f.readline().strip().split()
+    sarakkeet = {int(i): [int(eka_rivi[i])] for i in range(len(eka_rivi))}
+    for r in f:
+        rivi = r.strip().split()
+        if rivi[0][0] in "0123456789":
+            for i in range(len(rivi)):
+                sarakkeet[i].append(int(rivi[i]))
+        else:
+            for i in range(len(rivi)):
+                operaattorit = {i: rivi[i] for i in range(len(rivi))}
+
+kaikkiaan = 0
+for i in range(len(sarakkeet)):
+    if operaattorit[i] == "+":
+        kaikkiaan += sum(sarakkeet[i])
+    elif operaattorit[i] == "*":
+        kaikkiaan += prod(sarakkeet[i])
+
+print(kaikkiaan)
+
