8859390706a4b435860a88d256317d0adca1397e
sloproo <3440817+sloproo@users.noreply.github.com>
2025-12-06 15:21:14 +0200

    Toka nätimmin 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮 🇫🇮

--- 06/02.py
+++ 06/02.py
@@ -4,33 +4,25 @@ with open("input.txt") as f:
     rivit = []
     for r in f:
         if r.lstrip()[0] in "1234567890":
-            rivit.append(r.replace("\n", ""))
+            rivit.append(r.replace("\n", "L"))
         else:
-            operaattorit = r.replace("\n", "")
+            operaattorit = r.replace("\n", "L")
 
-kaikkiaan = []
+kaikkiaan = 0
 pystyt = []
-for i in range(len(rivit[0]) + 1):
-    if i >= len(rivit[0]):
-        if nyk_operaattori == "+":
-            kaikkiaan.append(sum(pystyt))
-        elif nyk_operaattori == "*":
-            kaikkiaan.append(prod(pystyt))
-        break
-
-    if operaattorit[i] != " ":
+for i in range(len(rivit[0])):
+    if operaattorit[i] == "+" or operaattorit[i] == "*":
         nyk_operaattori = operaattorit[i]
     pysty = "".join([rivi[i] for rivi in rivit])
-    if pysty == " " * len(rivit) or pysty == "\n" * len(rivit):
-        if nyk_operaattori == "+":
-            kaikkiaan.append(sum(pystyt))
-        elif nyk_operaattori == "*":
-            kaikkiaan.append(prod(pystyt))
-        pystyt = []
     
+    if pysty == " " * len(rivit) or pysty == "L" * len(rivit):
+        if nyk_operaattori == "+":
+            kaikkiaan += sum(pystyt)
+        elif nyk_operaattori == "*":
+            kaikkiaan += prod(pystyt)
+        pystyt = []
     else:
         pystyt.append(int(pysty))
         continue
 
 print(f"Kaikkien laskutoimitusten tulosten summa on {kaikkiaan}")
-print(sum(kaikkiaan))
