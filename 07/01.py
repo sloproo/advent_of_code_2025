cc57cf71175fadc1a5c5dfd3193a9c6a644e2dd3
sloproo <3440817+sloproo@users.noreply.github.com>
2025-12-07 09:57:15 +0200

    Seiskapäivä alkaa hyvin

--- 07/01.py
+++ 07/01.py
@@ -1,1 +1,21 @@
-# alku
+with open("input.txt") as f:
+    jakaja =[]
+    for r in f:
+        jakaja.append(r.strip())
+
+sateet = {jakaja[0].find("S")}
+halkaisuja = 0
+
+for r in jakaja:
+    uudet_sateet = set()
+    for sade in sateet:
+        if r[sade] == "^":
+            uudet_sateet.update({sade-1, sade+1})
+            halkaisuja += 1
+        else:
+            uudet_sateet.add(sade)
+    sateet = uudet_sateet
+
+print(halkaisuja)
+    
+
