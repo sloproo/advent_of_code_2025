6ed6a0081805c899abd0d3b7195ae48e104d1a0d
sloproo <3440817+sloproo@users.noreply.github.com>
2025-12-09 07:59:06 +0200

    tokaan pohja

--- 09/02.py
+++ 09/02.py
@@ -1,1 +1,13 @@
-# alku
+from itertools import combinations
+
+tiedosto = "input.txt"
+
+with open(tiedosto) as f:
+    kulmat = [tuple(int(luku) for luku in (r.strip().split(","))) for r in f]
+
+suurin = 0
+for eka, toka in combinations(kulmat, 2):
+    if (ala := (abs(eka[0] - toka[0]) + 1) * (abs(eka[1] - toka[1]) +1)) > suurin:
+        suurin = ala
+
+print(f"Suurin pinta-ala kahden ruudun kulmista on {suurin}")
