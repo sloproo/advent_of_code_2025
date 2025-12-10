2b129d400b60c6f403f5748b0e6274382f7da82e
sloproo <3440817+sloproo@users.noreply.github.com>
2025-12-11 00:54:11 +0200

    jotain yhtälön / yhtälömatriisin ratkaisua tähän pitäisi keksiä

--- 10/02.py
+++ 10/02.py
@@ -1,1 +1,54 @@
-# alku
+import itertools
+
+def lue(tiedosto: str) -> list:
+    koneet = []
+    with open(tiedosto) as f:
+        for r in f:
+            r = r.strip().split(" ")
+            valot  = []
+            valo = r[0][1:-1]
+            for i in range(len(valo)):
+                valot.append(int(i)) if valo[i] == "#" else i
+            
+            napit = []
+            for nappi in r[1:-1]:
+                napit.append([int(luku) for luku in nappi[1:-1].split(",")]) 
+
+            joltit = [int(i) for i in r[-1][1:-1].split(",")]
+            koneet.append({"valot": valot, "napit": napit, "joltit": joltit})
+    return koneet
+
+def vahimmat_painallukset(kone: dict) -> int:
+    painalluksia_vahintaan = max(kone["joltit"])
+    for i in range(painalluksia_vahintaan, 1000):
+        kokeiltavat_yhdistelmat = list(itertools.combinations_with_replacement(kone["napit"], i))
+        jannitteet = [0 for _ in range(len(kone["joltit"]))]
+        for nappiyhdistelma in kokeiltavat_yhdistelmat:
+            painettavat_napit = set([luku for nappi in nappiyhdistelma for luku in nappi])
+            nousevat_joltit = set(j for j in range(len(kone["joltit"])) if kone["joltit"][j] != 0)
+            if painettavat_napit != nousevat_joltit:
+                continue
+            for nappi in nappiyhdistelma:
+                for luku in nappi:
+                    jannitteet[luku] += 1
+                    if jannitteet[luku] > kone["joltit"][luku]:
+                        break
+                break
+            
+            if jannitteet == kone["joltit"]:
+                print(f"Ratkaistu kone, jossa oli {len(kone['joltit'])} patteria")
+                print(f"painalluksia vaadittiin {i}")
+                return i
+            else:
+                jannitteet = [0 for _ in range(len(kone["joltit"]))]
+    raise EOFError("Ei löytynyt toimivaa nappiyhdistelmää")
+            
+koneet = lue("input.txt")
+
+painalluksia = 0
+for kone in koneet:
+    painalluksia += vahimmat_painallukset(kone)
+print(f"Kaikkien koneiden kaikkien lamppujen sytyttämiseen riittää " +
+      f"{painalluksia} napinpainallusta")
+print(f"Eli siis {painalluksia}\n")
+print(painalluksia)
