52ab712c51b0e008b35a467270b8b5518f475330
sloproo <3440817+sloproo@users.noreply.github.com>
2025-12-10 19:24:22 +0200

    JES JES JES

--- 09/02.py
+++ 09/02.py
@@ -56,23 +56,48 @@ def suunta(alku: tuple[int, int], loppu: tuple[int, int]) -> str:
 def tormaako (v1_alku: tuple, v1_loppu: tuple, v2_alku: tuple, v2_loppu: tuple) -> bool:
     if suunta(v1_alku, v1_loppu) == suunta(v2_alku, v2_loppu):
         return False
-    ekan_pisteet = viivan_pisteet(v1_alku, v1_loppu)
-    tokan_pisteet = viivan_pisteet(v2_alku, v2_loppu)
-    for piste in ekan_pisteet:
-        if piste in tokan_pisteet:
-            paanaapurit = [(piste[0] -1, piste[1]), (piste[0] +1, piste[1]), \
-                           (piste[0], piste[1] -1), (piste[0], piste[1] +1)]
-            if all(naapuri in ekan_pisteet and naapuri in tokan_pisteet \
-                   for naapuri in paanaapurit): # Tässä on riski
-                return True
-            else:
-                return False
+    if suunta(v1_alku, v1_loppu) == "V":
+        vert = (v1_alku, v1_loppu)
+        hori = (v2_alku, v2_loppu)
+    elif suunta(v1_alku, v1_loppu) == "H":
+        hori = (v1_alku, v1_loppu)
+        vert = (v2_alku, v2_loppu)
+    else:
+        raise ValueError("Viivat omituisesti eivät saman- eikä poikkisuuntaiset")
+    if min(vert[0][1], vert[1][1]) < hori[0][1] < max(vert[0][1], vert[1][1]) and \
+    min(hori[0][0], hori[1][0]) < vert[0][0] < max(hori[0][0], hori[1][0]):
+        return True
     else:
         return False
 
+def maksimit(kulmat: list) -> tuple[int, int]:
+    max_x = 0
+    max_y = 0
+    for jana in janat:
+        max_x = max(max_x, jana[0][0], jana[1][0])
+        max_y = max(max_y, jana[0][1], jana[1][1])
+    return (max_x, max_y)
+
+def onko_sisalla(x: int, y: int, janat: list, max_x: int, max_y: int) -> bool:
+    px = x + 0.5 if x < max_x // 2 else x - 0.5
+    py = y + 0.5 if y < max_y // 2 else y - 0.5
+    osumia = 0
+
+    for jana in janat:
+        if suunta(jana[0], jana[1]) == "H":
+            continue
+        else:
+            if tormaako((px, py), (max_x * 2, py), jana[0], jana[1]):
+                osumia += 1
+    return True if osumia % 2 == 1 else False
+
+
 def onko_mahdollinen(eka: tuple[int, int], toka: tuple[int, int], kulmat: list, 
-                     janat: list) -> bool:
+                     janat: list, max_x: int, max_y: int) -> bool:
     if kulmia_sisalla(eka, toka, kulmat):
+        return False
+    if not onko_sisalla(((eka[0] + toka[0]) // 2), ((eka[1] + toka[1]) // 2), \
+                        janat, max_x, max_y):
         return False
     viivakoordinaatit = viisto_viivakoordinaateiksi(eka, toka)
     for viiva in viivakoordinaatit:
@@ -85,9 +110,10 @@ def onko_mahdollinen(eka: tuple[int, int], toka: tuple[int, int], kulmat: list,
 
 
 if __name__ == "__main__":
-    tiedosto = "input.txt"
+    tiedosto = "alku.txt"
     kulmat = lue_kulmat(tiedosto)
     janat = tee_janat(kulmat)
+    max_x, max_y = maksimit(kulmat)
     kulmat.sort(key= lambda kulma: kulma[0])
     suurin_ala = 0
 
@@ -101,7 +127,7 @@ if __name__ == "__main__":
         piste_1, piste_2 = alat[i][0:2]
         print(f"Käydään läpi kulmayhdistelmä #{i}, suurin ala tähän asti "
               + f"on {suurin_ala}")
-        if not onko_mahdollinen(piste_1, piste_2, kulmat, janat):
+        if not onko_mahdollinen(piste_1, piste_2, kulmat, janat, max_x, max_y):
             continue
         else:
             nykyisen_ala = ala(piste_1, piste_2)
@@ -110,63 +136,3 @@ if __name__ == "__main__":
                 suurin_ala = nykyisen_ala
     
     print(f"Huh, kaikki käyty läpi ja suurin ala on {suurin_ala}")
-
-
-# 4598136816 liian iso
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def suunta(eka: tuple[int, int], toka: tuple[int, int]) -> str:
-#     dx = eka[0] - toka[0]
-#     dy = eka[1] - toka[1]
-#     if dx != 0 and dy != 0:
-#         raise ValueError("Viiva pisteiden välillä ei ole vaaka- tai pystysuora")
-#     elif dx != 0:
-#         return "R" if dx > 0 else "L"
-#     elif dy != 0:
-#         return "U" if dy > 0 else "D"
-
-def avoimet_suunnat(kulmat: list) -> dict:
-    aukeavat = {"UR": ["D", "R", "DR"]} # kesken
-    avoimet = {}
-    for i in range(len(kulmat) -1):
-        taitos = suunta(kulmat[i-1], kulmat[i]) + suunta(kulmat[i], kulmat[i+1])
-        avoimet[kulmat[i]] = taitos
-    taitos = suunta(kulmat[-2], kulmat[-1]) + suunta(kulmat[-1], kulmat[0])
-    avoimet[kulmat[-1]] = suunta(kulmat[-2], kulmat[-1]) + suunta(kulmat[-1], kulmat[0])
-    return avoimet
