b5940f1c8f0d465c2f65c4d0212e69538937a3f3
sloproo <3440817+sloproo@users.noreply.github.com>
2025-12-11 19:06:36 +0200

    11. päivän eka woop ��

--- 11/01.py
+++ 11/01.py
@@ -1,1 +1,29 @@
-# alku
+def avaa(tiedosto: str) -> dict:
+    sanakirja = {}
+    with open(tiedosto) as f:
+        for r in f:
+            sanakirja[r.split(": ")[0]] = r.split(": ")[1].strip().split(" ")
+    return sanakirja
+
+def syvemmalle(portti: str, portit: dict, ratkaistut: dict, kaydyt: list) -> tuple[int, dict, list]:
+    reitteja  = 0
+    for maali in portit[portti]:
+        if maali in ratkaistut:
+            reitteja += ratkaistut[maali]
+        elif maali == "out":
+            reitteja += 1
+        else:
+            reitteja += syvemmalle(maali, portit, ratkaistut, kaydyt)[0]
+    ratkaistut[portti] = reitteja
+    return (reitteja, ratkaistut, kaydyt)
+
+portit = avaa("input.txt")
+kaydyt = ["you"]
+reitteja = 0
+ratkaistut = {}
+
+for portti in portit["you"]:
+    uusiareitteja, ratkaistut, kaydyt = syvemmalle(portti, portit, ratkaistut, kaydyt)
+    reitteja += uusiareitteja
+
+print(reitteja)
--- 11/02.py
+++ 11/02.py
@@ -1,1 +1,32 @@
-# alku
+def avaa(tiedosto: str) -> dict:
+    sanakirja = {}
+    with open(tiedosto) as f:
+        for r in f:
+            sanakirja[r.split(": ")[0]] = r.split(": ")[1].strip().split(" ")
+    return sanakirja
+
+def syvemmalle(portti: str, portit: dict, ratkaistut: dict, kaydyt: list) -> tuple[int, dict, list]:
+    reitteja  = 0
+    for maali in portit[portti]:
+        if maali in ratkaistut:
+            reitteja += ratkaistut[maali]
+        elif maali == "out":
+            if "fft" in kaydyt and "dac" in kaydyt:
+                reitteja += 1
+            else:
+                reitteja += 0
+        else:
+            reitteja += syvemmalle(maali, portit, ratkaistut, kaydyt)[0]
+    ratkaistut[portti] = reitteja
+    return (reitteja, ratkaistut, kaydyt)
+
+portit = avaa("alku2.txt")
+kaydyt = ["svr"]
+reitteja = 0
+ratkaistut = {}
+
+for portti in portit["svr"]:
+    uusiareitteja, ratkaistut, kaydyt = syvemmalle(portti, portit, ratkaistut, kaydyt)
+    reitteja += uusiareitteja
+
+print(reitteja)
