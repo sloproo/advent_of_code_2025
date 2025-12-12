b7a444465c08b17119052edbdad7d8d9e7ae0f73
sloproo <3440817+sloproo@users.noreply.github.com>
2025-12-12 02:17:30 +0200

    Kesken on en tiiä

--- 11/02.py
+++ 11/02.py
@@ -5,28 +5,54 @@ def avaa(tiedosto: str) -> dict:
             sanakirja[r.split(": ")[0]] = r.split(": ")[1].strip().split(" ")
     return sanakirja
 
-def syvemmalle(portti: str, portit: dict, ratkaistut: dict, kaydyt: list) -> tuple[int, dict, list]:
-    reitteja  = 0
-    for maali in portit[portti]:
-        if maali in ratkaistut:
-            reitteja += ratkaistut[maali]
-        elif maali == "out":
-            if "fft" in kaydyt and "dac" in kaydyt:
-                reitteja += 1
-            else:
-                reitteja += 0
+def pitkanimi(portti: str, dacfft: list[int]):
+    return portti + "".join([str(i) for i in dacfft])
+
+def syvemmalle(portti: str, portit: dict, ratkaistut: dict, kaydyt: list, 
+               dacfft: list[int]) -> tuple[int, dict]:
+    reitteja = 0
+    if portti == "dac":
+        dacfft[0] = 1
+    elif portti == "fft":
+        dacfft[1] = 1
+    nyk_pitka = pitkanimi(portti, dacfft)
+    kaydyt.append(nyk_pitka)
+    if portti == "out":
+        if dacfft == [1, 1]:
+            reitteja += 1
+            print(f"Löytyi kunnon reitti {kaydyt}")
         else:
-            reitteja += syvemmalle(maali, portit, ratkaistut, kaydyt)[0]
-    ratkaistut[portti] = reitteja
-    return (reitteja, ratkaistut, kaydyt)
+            reitteja += 0
+            # print(f"Lopussa muttei dacfft {kaydyt}")
+        
+        return (reitteja, ratkaistut)
+    else:
+        for maali in portit[portti]:
+            if portti == "out":
+                pass
+            pitka = pitkanimi(maali, dacfft)
+            if pitka in ratkaistut:
+                reitteja += ratkaistut[pitka]
+            if pitka in kaydyt:
+                continue
+            else:
+                reitteja, ratkaistut = syvemmalle(maali, portit, ratkaistut, kaydyt, dacfft)
+                
+    ratkaistut[pitka] = reitteja
+    return (reitteja, ratkaistut)
 
 portit = avaa("alku2.txt")
-kaydyt = ["svr"]
+kaydyt = [("svr00")]
 reitteja = 0
 ratkaistut = {}
+dacfft = [0, 0]
+# Halutut portit: "dac" ja "fft"
 
 for portti in portit["svr"]:
-    uusiareitteja, ratkaistut, kaydyt = syvemmalle(portti, portit, ratkaistut, kaydyt)
-    reitteja += uusiareitteja
+    uusia_reitteja, ratkaistut = syvemmalle(portti, portit, ratkaistut, kaydyt, [0, 0])
+    reitteja += uusia_reitteja
 
 print(reitteja)
+
+# 22 väärin
+# 316 liian matala
--- 12/01.py
+++ 12/01.py
@@ -1,1 +1,3 @@
-# alku
+def pitkanimi(joo: str, hoo: list[int]):
+    return joo + "".join(str(i) for i in hoo)
+print(pitkanimi("jooasfasf", [4, 5, 6]))
