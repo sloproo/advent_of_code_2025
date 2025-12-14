e136cd50a2cb171e015ffad75ca2ed7a5426d3d3
sloproo <3440817+sloproo@users.noreply.github.com>
2025-12-14 19:40:49 +0200

    ortools-tiedostolle oikea pääte

--- 12/01.py
+++ 12/01.py
@@ -1,3 +1,20 @@
-with open("alku.txt", encoding="utf-8") as f:
+with open("input.txt") as f:
+    palat = []
     for r in f:
-        print(r.strip(), end=", ")
+        if r == "\n":
+            continue
+        if r[1] == ":":
+            palanro = int(r[0])
+            pala = [f.readline().strip() for _ in range(3)]
+            palat.append(pala)
+        if palanro == 5:
+            f.readline()
+            break
+    kuuset = []
+    for r in f:
+        kenttapuoli, palapuoli = r.split(": ")
+        ala = tuple(int(x) for x in kenttapuoli.split("x"))
+        palat = [int(l) for l in palapuoli.strip().split(" ")]
+        kuuset.append((ala, palat))
+
+pass
