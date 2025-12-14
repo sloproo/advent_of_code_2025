8ca3a72b012c8e16f004de599082a98337365d73
sloproo <3440817+sloproo@users.noreply.github.com>
2025-12-14 22:41:42 +0200

    kesken rivin vika

--- 12/01.py
+++ 12/01.py
@@ -1,20 +1,34 @@
-with open("input.txt") as f:
-    palat = []
-    for r in f:
-        if r == "\n":
-            continue
-        if r[1] == ":":
-            palanro = int(r[0])
-            pala = [f.readline().strip() for _ in range(3)]
-            palat.append(pala)
-        if palanro == 5:
-            f.readline()
-            break
-    kuuset = []
-    for r in f:
-        kenttapuoli, palapuoli = r.split(": ")
-        ala = tuple(int(x) for x in kenttapuoli.split("x"))
-        palat = [int(l) for l in palapuoli.strip().split(" ")]
-        kuuset.append((ala, palat))
+def lue(tiedosto: str):
+    with open("input.txt") as f:
+        palat = []
+        for r in f:
+            if r == "\n":
+                continue
+            if r[1] == ":":
+                palanro = int(r[0])
+                pala = [f.readline().strip() for _ in range(3)]
+                palat.append(pala)
+            if len(palat) == 6:
+                f.readline()
+                break
+        palat = tuple(palat)
+        kuuset = []
+        for r in f:
+            kenttapuoli, palapuoli = r.split(": ")
+            ala = tuple(int(x) for x in kenttapuoli.split("x"))
+            palalista = [int(l) for l in palapuoli.strip().split(" ")]
+            kuuset.append((ala, palalista))
+    return palat, kuuset
 
-pass
+palat, kuuset = lue("input.txt")
+
+ruutumaarat = []
+for pala in palat:
+    ruudut = "".join([rivi for rivi in pala]).count("#")
+    ruutumaarat.append(ruudut)
+ruutumaarat = tuple(ruutumaarat)
+
+for kuusi in kuuset:
+    tilaa = kuusi[0][0] * kuusi[0][1]
+    ruutuja_vaaditaan = ruutumaarat[i] * 
+
